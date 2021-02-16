073363ed7635d6e1c0c13babaa615b74e6ea85cb
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-02-16 08:11:55 -0600

    GroupProject

---
+++ GroupProject/Data/GroupProjectContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project8_GroupProject.Models;
+
+namespace Project8_GroupProject.Data
+{
+    /// <summary>
+    /// Used to define the "context" for our database
+    /// </summary>
+    public class GroupProjectContext : DbContext
+    {
+        /// <summary>
+        /// Allows the program to interact with the database
+        /// </summary>
+        /// <param name="options">Represents the options that will be used by DbContext</param>
+        public GroupProjectContext(DbContextOptions<GroupProjectContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Represents the Accounts table 
+        /// </summary>
+        public DbSet<Accounts> Accounts { get; set; }
+
+        // Add a property for the Courses model here
+    }
+}
