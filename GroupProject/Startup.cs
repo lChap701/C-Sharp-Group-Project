2ca696e3b3cd6115ef1ca7a0e5d4bba840f9b5f4
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-02-16 08:24:05 -0600

    Finished bring changes over to repository

--- GroupProject/Startup.cs
+++ GroupProject/Startup.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Project8_GroupProject.Data;
 
-namespace GroupProject
+namespace Project8_GroupProject
 {
     public class Startup
     {
@@ -24,6 +21,11 @@ namespace GroupProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddControllersWithViews();
+
+            object p = services.AddDbContext<GroupProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("GroupProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
