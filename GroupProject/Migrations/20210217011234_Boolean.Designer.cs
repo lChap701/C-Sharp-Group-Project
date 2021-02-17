5223b586edf690c0708b2fb0c2552ce674d4798e
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-02-16 20:39:40 -0600

    Added migrations

--- GroupProject/Migrations/20210217011234_Boolean.Designer.cs
+++ GroupProject/Migrations/20210217011234_Boolean.Designer.cs
@@ -3,14 +3,16 @@ using GroupProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GroupProject.Migrations
 {
     [DbContext(typeof(GroupProjectContext))]
-    partial class GroupProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20210217011234_Boolean")]
+    partial class Boolean
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GroupProject/Migrations/20210217011234_Boolean.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupProject.Migrations
+{
+    public partial class Boolean : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAdmin",
+                table: "Accounts",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsAdmin",
+                table: "Accounts",
+                type: "nvarchar(1)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
