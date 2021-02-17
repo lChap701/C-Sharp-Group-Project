c6078d374782a20e7a31be59fc73b3ae7696adbb
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-02-16 20:40:10 -0600

    Removed EncryptPassword() method

--- GroupProject/Controllers/CoursesController.cs
+++ GroupProject/Controllers/CoursesController.cs
@@ -61,13 +61,12 @@ namespace GroupProject.Controllers
         /// <param name="password">Represents the password that was submitted</param>
         /// <returns>Returns the view</returns>
         [HttpPost, ValidateAntiForgeryToken]
-        public ActionResult Login(string user, string password)
+        public ActionResult Login(string username, string password)
         {
             // Checks if any errors occurred
             if (ModelState.IsValid)
             {
-                password = EncryptPassword(password);
-                var data = _context.Accounts.Where(s => (s.Email.Equals(user) || s.Username.Equals(user)) && s.Password.Equals(password)).ToList();
+                var data = _context.Accounts.Where(s => (s.Username.Equals(username)) && s.Password.Equals(password)).ToList();
 
                 // Checks if an account was found
                 if (data.Count > 0)
@@ -76,8 +75,8 @@ namespace GroupProject.Controllers
                 }
                 else
                 {
-                    ViewBag["Error"] = "Account was not found";
-                    return RedirectToAction("Login");
+                    ViewBag.Error = "Account was not found";
+                    return View();
                 }
             }
 
@@ -111,40 +110,18 @@ namespace GroupProject.Controllers
                 // Checks if an email address was found 
                 if (check != null)
                 {
-                    _user.Password = EncryptPassword(_user.Password);
                     _context.Accounts.Add(_user);
                     _context.SaveChanges();
-                    return RedirectToAction("Index");
+                    return RedirectToAction("Login");
                 }
                 else
                 {
-                    ViewBag["Error"] = "Email address is not unique";
+                    ViewBag.Error = "Email address is not unique";
                     return View();
                 }
             }
 
             return View();
-        }
-
-        /// <summary>
-        /// Encrypts sensitive data (such as passwords)
-        /// </summary>
-        /// <param name="psw">Represents the password that should be encrypted</param>
-        /// <returns>Returns the encrypted password</returns>
-        public static string EncryptPassword(string psw)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            byte[] fromData = Encoding.UTF8.GetBytes(psw);
-            byte[] targetData = md5.ComputeHash(fromData);
-            string hash = null;
-
-            // Encrypts the data
-            for (int i = 0; i < targetData.Length; i++)
-            {
-                hash += targetData[i].ToString("x2");
-            }
-
-            return hash;
         }
     }
 }
