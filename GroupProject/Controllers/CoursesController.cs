36366b1d32f815ae7575ce89dd94c655408894ca
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-02-17 20:10:38 -0600

    Improved validation

--- GroupProject/Controllers/CoursesController.cs
+++ GroupProject/Controllers/CoursesController.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using GroupProject.Data;
 using GroupProject.Models;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Text.RegularExpressions;
 
 namespace GroupProject.Controllers
 {
@@ -12,6 +14,7 @@ namespace GroupProject.Controllers
     public class CoursesController : Controller
     {
         private readonly GroupProjectContext _context;
+        private static readonly Regex regex = new Regex(@"[^a-zA-Z]");  // used to double check results
 
         /// <summary>
         /// Sets the data contained in a GroupProjectContext object to a new object
@@ -22,7 +25,7 @@ namespace GroupProject.Controllers
             _context = context;
         }
 
-        // GET: /Courses
+        // GET: Courses
         /// <summary>
         /// Displays the Index view (the Catalog Page)
         /// </summary>
@@ -37,7 +40,7 @@ namespace GroupProject.Controllers
          * https://dev.to/skipperhoa/login-and-register-using-asp-net-mvc-5-3i0g
          */
 
-        // GET: /Courses/Login
+        // GET: Courses/Login
         /// <summary>
         /// Displays the Login page/view
         /// </summary>
@@ -47,7 +50,7 @@ namespace GroupProject.Controllers
             return View();
         }
 
-        // POST: /Courses/Login
+        // POST: Courses/Login
         /// <summary>
         /// Attempts to log the user in to the Course Catolog
         /// </summary>
@@ -60,15 +63,37 @@ namespace GroupProject.Controllers
             // Checks if any errors occurred
             if (ModelState.IsValid)
             {
-                var data = _context.Accounts.Where(s => (s.Username.Equals(username)) && s.Password.Equals(password)).ToList();
+                var data = _context.Accounts.Where(s => s.Username.Equals(username.Trim()) && s.Password.Equals(password.Trim()))
+                            .ToList();
 
                 // Checks if an account was found
                 if (data.Count > 0)
                 {
-                    var user = _context.Accounts.Where(s => (s.Username.Equals(username)) && s.Password.Equals(password)).ToList().Single();
-                    HttpContext.Session.SetString("admin", user.IsAdmin.ToString());
+                    var user = _context.Accounts.Where(s => s.Username.Equals(username.Trim()) && s.Password.Equals(password.Trim())).ToList().Single();
 
-                    return RedirectToAction("Index");
+                    // Double checks fields
+                    if (regex.Replace(username, string.Empty) == regex.Replace(user.Username, string.Empty))
+                    {
+                        if (regex.Replace(password, string.Empty) == regex.Replace(user.Password, string.Empty))
+                        {
+                            HttpContext.Session.SetString("admin", user.IsAdmin.ToString());
+
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            ViewBag.Error = "Password is incorrect";
+
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Username is incorrect";
+
+                        return View();
+                    }
+
                 }
                 else
                 {
@@ -81,7 +106,7 @@ namespace GroupProject.Controllers
             return View();
         }
 
-        // GET: /Courses/Signup
+        // GET: Courses/Signup
         /// <summary>
         /// Displays the Signup page/view
         /// </summary>
@@ -91,7 +116,7 @@ namespace GroupProject.Controllers
             return View();
         }
 
-        // POST: /Courses/Signup
+        // POST: Courses/Signup
         /// <summary>
         /// Attempts to sign up users for the Course Catolog
         /// </summary>
@@ -103,7 +128,7 @@ namespace GroupProject.Controllers
             // Check if any errors occur
             if (ModelState.IsValid)
             {
-                var check = _context.Accounts.Where(a => a.Email == _user.Email || a.Username == _user.Username).ToList();
+                var check = _context.Accounts.Where(a => a.Email.Equals(_user.Email) || a.Username.Equals(_user.Username)).ToList();
 
                 // Checks if an email address and username was found 
                 if (check.Count == 0)
@@ -115,8 +140,31 @@ namespace GroupProject.Controllers
                 }
                 else
                 {
-                    ViewBag.Error = "Email address and username should be unique";
-                    return View();
+                    var errorCheck = _context.Accounts.Where(a => a.Email.Equals(_user.Email) || a.Username.Equals(_user.Username)).Single();
+
+                    // Double check fields
+                    if (regex.Replace(_user.Username, string.Empty) != regex.Replace(errorCheck.Username, string.Empty))
+                    {
+                        if (regex.Replace(_user.Email, string.Empty) != regex.Replace(errorCheck.Email, string.Empty))
+                        {
+                            _context.Accounts.Add(_user);
+                            _context.SaveChanges();
+
+                            return RedirectToAction("Login");
+                        }
+                        else
+                        {
+                            ViewBag.Error = "Email address should be unique";
+
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Username should be unique";
+
+                        return View();
+                    }
                 }
             }
 
